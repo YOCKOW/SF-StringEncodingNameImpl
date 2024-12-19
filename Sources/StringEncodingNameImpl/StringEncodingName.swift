3647f3b5b7decb030b206ec8cdb822f52f16453d
YOCKOW <YOCKOW@users.noreply.github.com>
2024-12-19 15:51:52 +0900

    Add more available encoding names.

--- Sources/StringEncodingNameImpl/StringEncodingName.swift
+++ Sources/StringEncodingNameImpl/StringEncodingName.swift
@@ -33,29 +33,38 @@ extension String.Encoding {
   /// Creates an instance from the name of the IANA registry "charset".
   public init?(charsetName: String) {
     func __determineEncoding() -> String.Encoding? {
-      func __matches(_ charset: IANACharset) -> Bool {
-        return charset.matches(charsetName)
+      func __matches(oneOf charsets: IANACharset...) -> Bool {
+        assert(!charsets.isEmpty)
+        for charset in charsets {
+          if charset.matches(charsetName) {
+            return true
+          }
+        }
+        return false
       }
 
-      return if __matches(.utf8) { .utf8 }
-      else if __matches(.usASCII) { .ascii }
-      else if __matches(.eucJP) { .japaneseEUC }
-      else if __matches(.iso8859_1) { .isoLatin1 }
-      else if __matches(.shiftJIS) { .shiftJIS }
-      else if __matches(.iso8859_2) { .isoLatin2 }
-      else if __matches(.utf16) { .utf16 }
-      else if __matches(.windows1251) { .windowsCP1251 }
-      else if __matches(.windows1252) { .windowsCP1252 }
-      else if __matches(.windows1253) { .windowsCP1253 }
-      else if __matches(.windows1254) { .windowsCP1254 }
-      else if __matches(.windows1250) { .windowsCP1250 }
-      else if __matches(.iso2022JP) { .iso2022JP }
-      else if __matches(.macintosh) { .macOSRoman }
-      else if __matches(.utf16BE) { .utf16BigEndian }
-      else if __matches(.utf16LE) { .utf16LittleEndian }
-      else if __matches(.utf32) { .utf32 }
-      else if __matches(.utf32BE) { .utf32BigEndian }
-      else if __matches(.utf32LE) { .utf32LittleEndian }
+      return if __matches(oneOf: .utf8) { .utf8 }
+      else if __matches(oneOf: .usASCII, .iso646Irv1983) { .ascii }
+      else if __matches(oneOf: .eucJP) { .japaneseEUC }
+      else if __matches(oneOf: .iso8859_1) { .isoLatin1 }
+      else if __matches(oneOf: .adobeSymbolEncoding) { .symbol }
+      else if __matches(oneOf: .shiftJIS, .windows31J) { .shiftJIS }
+      else if __matches(oneOf: .iso8859_2) { .isoLatin2 }
+      else if __matches(oneOf: .utf16, .iso10646UCS2, .unicode1_1) { .utf16 }
+      else if __matches(oneOf: .windows1251) { .windowsCP1251 }
+      else if __matches(
+        oneOf: .windows1252, .iso8859_1Windows3_0Latin1, .iso8859_1Windows3_1Latin1
+      ) { .windowsCP1252 }
+      else if __matches(oneOf: .windows1253) { .windowsCP1253 }
+      else if __matches(oneOf: .windows1254, .iso8859_9WindowsLatin5) { .windowsCP1254 }
+      else if __matches(oneOf: .windows1250, .iso8859_2WindowsLatin2) { .windowsCP1250 }
+      else if __matches(oneOf: .iso2022JP) { .iso2022JP }
+      else if __matches(oneOf: .macintosh) { .macOSRoman }
+      else if __matches(oneOf: .utf16BE) { .utf16BigEndian }
+      else if __matches(oneOf: .utf16LE) { .utf16LittleEndian }
+      else if __matches(oneOf: .utf32, .iso10646UCS4) { .utf32 }
+      else if __matches(oneOf: .utf32BE) { .utf32BigEndian }
+      else if __matches(oneOf: .utf32LE) { .utf32LittleEndian }
       else { nil }
     }
 
